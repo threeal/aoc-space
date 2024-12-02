c399150a50df8df1cf4814284cd93f5c88f71acd
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-02 13:51:38 +0700

    feat(day-1): solve second puzzle by using hash map to count frequencies
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- day-01/puzzle-2.cpp
+++ day-01/puzzle-2.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 int main() {
+  std::vector<int> first{};
+  std::unordered_map<int, int> second{};
+
   std::string line{};
   while (std::getline(std::cin, line)) {
+    std::istringstream iss(line);
+
+    int num{};
+    iss >> num;
+    first.push_back(num);
+
+    iss >> num;
+    auto it = second.find(num);
+    if (it == second.end()) {
+      second.emplace(num, 1);
+    } else {
+      ++(it->second);
+    }
   }
+
+  int total{0};
+  for (const auto num : first) {
+    const auto it = second.find(num);
+    if (it != second.end()) total += num * it->second;
+  }
+
+  std::cout << total << "\n";
+
   return 0;
 }
