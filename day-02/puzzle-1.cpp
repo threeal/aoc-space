3c312647312a728837ca19246285cc9f36b5433e
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-02 14:22:08 +0700

    feat(day-2): solve first puzzle by counting valid sequences
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- day-02/puzzle-1.cpp
+++ day-02/puzzle-1.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <sstream>
 #include <string>
 
 int main() {
   std::string line{};
+
+  int count{0};
   while (std::getline(std::cin, line)) {
+    std::istringstream iss(line);
+
+    int a{}, b{};
+    iss >> a >> b;
+
+    if (a < b) {
+    repeat_inc:
+      if (b - a <= 0 || b - a > 3) continue;
+      a = b;
+      if (iss >> b) goto repeat_inc;
+      ++count;
+    } else if (a > b) {
+    repeat_dec:
+      if (a - b <= 0 || a - b > 3) continue;
+      a = b;
+      if (iss >> b) goto repeat_dec;
+      ++count;
+    }
   }
+
+  std::cout << count << "\n";
+
   return 0;
 }
