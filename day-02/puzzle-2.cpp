d62b9f1789fe776b8e09f704c689a5dad838913a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-02 23:59:13 +0700

    feat(day-2): solve second puzzle by using brute force
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- day-02/puzzle-2.cpp
+++ day-02/puzzle-2.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
+#include <sstream>
 #include <string>
+#include <vector>
+
+bool check(const std::vector<int>& nums) {
+  if (nums[1] > nums[0]) {
+    if (nums[1] - nums[0] > 3) return false;
+    for (std::size_t i{2}; i < nums.size(); ++i) {
+      if (nums[i] <= nums[i - 1] || nums[i] - nums[i - 1] > 3) return false;
+    }
+    return true;
+  } else if (nums[1] < nums[0]) {
+    if (nums[1] - nums[0] < -3) return false;
+    for (std::size_t i{2}; i < nums.size(); ++i) {
+      if (nums[i] >= nums[i - 1] || nums[i] - nums[i - 1] < -3) return false;
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
 
 int main() {
+  int count{0};
+
   std::string line{};
   while (std::getline(std::cin, line)) {
+    std::istringstream iss(line);
+    std::vector<int> nums{};
+
+    int num{};
+    while (iss >> num) nums.push_back(num);
+
+    if (check(nums)) {
+      ++count;
+    } else {
+      for (std::size_t i{0}; i < nums.size(); ++i) {
+        auto other = nums;
+        other.erase(other.begin() + i);
+        if (check(other)) {
+          ++count;
+          break;
+        }
+      }
+    }
   }
+
+  std::cout << count << "\n";
+
   return 0;
 }
