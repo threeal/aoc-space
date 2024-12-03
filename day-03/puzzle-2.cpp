1eee6ceb87f2277b15c5086cfa26e19b457decc4
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-03 20:58:57 +0700

    feat(day-3): solve second puzzle by using switch to parse characters
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- day-03/puzzle-2.cpp
+++ day-03/puzzle-2.cpp
@@ -1,66 +1,104 @@
 #include <iostream>
 
 int main() {
+  bool enabled{true};
   int sum{0};
 
   char ch{};
-  while (std::cin.get(ch)) {
-    if (ch != 'm') continue;
-
-    if (!std::cin.get(ch)) break;
-    if (ch != 'u') continue;
-
-    if (!std::cin.get(ch)) break;
-    if (ch != 'l') continue;
-
-    if (!std::cin.get(ch)) break;
-    if (ch != '(') continue;
-
-    int a{0};
-    if (!std::cin.get(ch)) break;
-    if (ch >= '0' && ch <= '9') {
-      a += ch - '0';
-      if (!std::cin.get(ch)) break;
-      if (ch >= '0' && ch <= '9') {
-        a = a * 10 + ch - '0';
-        if (!std::cin.get(ch)) break;
-        if (ch >= '0' && ch <= '9') {
-          a = a * 10 + ch - '0';
+  if (std::cin.get(ch)) {
+    while (true) {
+      if (enabled) {
+        if (ch == 'd') {
           if (!std::cin.get(ch)) break;
-          if (ch != ',') continue;
-        } else if (ch != ',') {
-          continue;
-        }
-      } else if (ch != ',') {
-        continue;
-      }
-    } else {
-      continue;
-    }
+          if (ch != 'o') continue;
 
-    int b{0};
-    if (!std::cin.get(ch)) break;
-    if (ch >= '0' && ch <= '9') {
-      b += ch - '0';
-      if (!std::cin.get(ch)) break;
-      if (ch >= '0' && ch <= '9') {
-        b = b * 10 + ch - '0';
-        if (!std::cin.get(ch)) break;
-        if (ch >= '0' && ch <= '9') {
-          b = b * 10 + ch - '0';
+          if (!std::cin.get(ch)) break;
+          if (ch != 'n') continue;
+
+          if (!std::cin.get(ch)) break;
+          if (ch != '\'') continue;
+
+          if (!std::cin.get(ch)) break;
+          if (ch != 't') continue;
+
+          if (!std::cin.get(ch)) break;
+          if (ch != '(') continue;
+
           if (!std::cin.get(ch)) break;
           if (ch != ')') continue;
-        } else if (ch != ')') {
-          continue;
-        }
-      } else if (ch != ')') {
-        continue;
-      }
-    } else {
-      continue;
-    }
 
-    sum += a * b;
+          enabled = false;
+        } else if (ch == 'm') {
+          if (!std::cin.get(ch)) break;
+          if (ch != 'u') continue;
+
+          if (!std::cin.get(ch)) break;
+          if (ch != 'l') continue;
+
+          if (!std::cin.get(ch)) break;
+          if (ch != '(') continue;
+
+          int a{0};
+          if (!std::cin.get(ch)) break;
+          if (ch >= '0' && ch <= '9') {
+            a += ch - '0';
+            if (!std::cin.get(ch)) break;
+            if (ch >= '0' && ch <= '9') {
+              a = a * 10 + ch - '0';
+              if (!std::cin.get(ch)) break;
+              if (ch >= '0' && ch <= '9') {
+                a = a * 10 + ch - '0';
+                if (!std::cin.get(ch)) break;
+                if (ch != ',') continue;
+              } else if (ch != ',') {
+                continue;
+              }
+            } else if (ch != ',') {
+              continue;
+            }
+          } else {
+            continue;
+          }
+
+          int b{0};
+          if (!std::cin.get(ch)) break;
+          if (ch >= '0' && ch <= '9') {
+            b += ch - '0';
+            if (!std::cin.get(ch)) break;
+            if (ch >= '0' && ch <= '9') {
+              b = b * 10 + ch - '0';
+              if (!std::cin.get(ch)) break;
+              if (ch >= '0' && ch <= '9') {
+                b = b * 10 + ch - '0';
+                if (!std::cin.get(ch)) break;
+                if (ch != ')') continue;
+              } else if (ch != ')') {
+                continue;
+              }
+            } else if (ch != ')') {
+              continue;
+            }
+          } else {
+            continue;
+          }
+
+          sum += a * b;
+        }
+      } else if (ch == 'd') {
+        if (!std::cin.get(ch)) break;
+        if (ch != 'o') continue;
+
+        if (!std::cin.get(ch)) break;
+        if (ch != '(') continue;
+
+        if (!std::cin.get(ch)) break;
+        if (ch != ')') continue;
+
+        enabled = true;
+      }
+
+      if (!std::cin.get(ch)) break;
+    }
   }
 
   std::cout << sum << "\n";
