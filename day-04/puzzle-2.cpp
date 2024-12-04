b2509eb3c8b3e0ff831ecc1b5d8a4dc9bffe13d2
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-04 17:18:24 +0700

    feat(day-4): solve second puzzle by using for-loop
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- day-04/puzzle-2.cpp
+++ day-04/puzzle-2.cpp
@@ -8,5 +8,27 @@ int main() {
   while (std::getline(std::cin, line)) {
     lines.push_back(line);
   }
+
+  int count{0};
+  for (std::size_t i{2}; i < lines.size(); ++i) {
+    for (std::size_t j{2}; j < lines.size(); ++j) {
+      if (lines[i - 1][j - 1] != 'A') continue;
+      if (lines[i - 2][j - 2] == 'S' && lines[i][j] == 'M') {
+        if (lines[i][j - 2] == 'S' && lines[i - 2][j] == 'M') {
+          ++count;
+        } else if (lines[i][j - 2] == 'M' && lines[i - 2][j] == 'S') {
+          ++count;
+        }
+      } else if (lines[i - 2][j - 2] == 'M' && lines[i][j] == 'S') {
+        if (lines[i][j - 2] == 'S' && lines[i - 2][j] == 'M') {
+          ++count;
+        } else if (lines[i][j - 2] == 'M' && lines[i - 2][j] == 'S') {
+          ++count;
+        }
+      }
+    }
+  }
+
+  std::cout << count << "\n";
   return 0;
 }
