8d5ad5005061fae5168acaaed6c7af430df46276
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-11 20:46:22 +0700

    feat: solve Day 7: Bridge Repair - second puzzle (#36)
    * test(day-7): add second puzzle
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    * feat(day-7): solve second puzzle by handling concatenation operation
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>
    
    ---------
    
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

---
+++ day-07/puzzle-2.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <queue>
+#include <string>
+
+int main() {
+  long long total{0};
+
+  std::string line{};
+  while (std::getline(std::cin, line)) {
+    std::size_t i{0};
+
+    long long test{0};
+    while (line[i] != ':') {
+      test = test * 10 + line[i] - '0';
+      ++i;
+    }
+
+    ++i;
+    while (line[i] < '0') ++i;
+
+    long long num{0};
+    while (line[i] >= '0') {
+      num = num * 10 + line[i] - '0';
+      ++i;
+    }
+
+    std::queue<long long> nums{};
+    nums.push(num);
+
+    while (i < line.size()) {
+      while (line[i] < '0') ++i;
+
+      num = 0;
+      while (line[i] >= '0') {
+        num = num * 10 + line[i] - '0';
+        ++i;
+      }
+
+      for (std::size_t n{nums.size()}; n > 0; --n) {
+        if (nums.front() + num <= test) nums.push(nums.front() + num);
+        if (nums.front() * num <= test) nums.push(nums.front() * num);
+
+        long long exp{10};
+        while (exp <= num) exp *= 10;
+        if (nums.front() * exp + num <= test) {
+          nums.push(nums.front() * exp + num);
+        }
+
+        nums.pop();
+      }
+    }
+
+    while (!nums.empty()) {
+      if (nums.front() == test) {
+        total += test;
+        break;
+      }
+      nums.pop();
+    }
+  }
+
+  std::cout << total << "\n";
+  return 0;
+}
