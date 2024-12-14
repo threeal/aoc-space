e80335bc3a5be934052ef78ce7c5ffb6265404c7
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-14 23:46:31 +0700

    DONT MERGE

--- day-14/puzzle-2.cpp
+++ day-14/puzzle-2.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 #include <string>
 
+struct Robot {
+  int x;
+  int y;
+  int vx;
+  int vy;
+
+  void move() {
+    x = (101 + x + vx) % 101;
+    y = (103 + y + vy) % 103;
+  }
+};
+
 int main() {
-  int quadrants[4] = {0};
+  std::vector<Robot> robots{};
+
   std::string line{};
   while (std::getline(std::cin, line)) {
     int x{0};
@@ -46,27 +59,64 @@ int main() {
     }
     if (neg) vy = -vy;
 
-    x = (x + vx * 100) % 101;
-    if (x < 0) x += 101;
-
-    y = (y + vy * 100) % 103;
-    if (y < 0) y += 103;
-
-    if (x < 50) {
-      if (y < 51) {
-        ++quadrants[0];
-      } else if (y > 51) {
-        ++quadrants[1];
-      }
-    } else if (x > 50) {
-      if (y < 51) {
-        ++quadrants[2];
-      } else if (y > 51) {
-        ++quadrants[3];
-      }
-    }
+    robots.push_back({.x = x, .y = y, .vx = vx, .vy = vy});
   }
 
-  std::cout << quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3] << '\n';
+  int step{1};
+  while (true) {
+    std::vector<std::string> lines(103, std::string(101, '.'));
+    for (auto& robot : robots) {
+      robot.move();
+      lines[robot.y][robot.x] = 'X';
+    }
+
+    for (std::size_t by{0}; by < lines.size() - 1; ++by) {
+      for (std::size_t bx{0}; bx < lines[by].size() - 1; ++bx) {
+        if (lines[by][bx] != 'X') continue;
+
+        if (lines[by][bx + 1] != 'X') continue;
+        if (lines[by + 1][bx] != 'X') continue;
+
+        std::size_t ex{bx + 1};
+        while (ex + 1 < lines[by].size() && lines[by][ex + 1] == 'X') ++ex;
+
+        std::size_t ey{by + 1};
+        while (ey + 1 < lines.size() && lines[ey + 1][bx] == 'X') ++ey;
+
+        bool good{true};
+        for (std::size_t x{bx}; x <= ex; ++x) {
+          if (lines[ey][x] != 'X') {
+            good = false;
+            break;
+          }
+        }
+        if (!good) continue;
+
+        for (std::size_t y{by}; y <= ey; ++y) {
+          if (lines[y][ex] != 'X') {
+            good = false;
+            break;
+          }
+        }
+        if (!good) continue;
+
+        std::size_t count{0};
+        for (std::size_t y{by}; y <= ey; ++y) {
+          for (std::size_t x{bx}; x <= ex; ++x) {
+            if (lines[y][x] == 'X') ++count;
+          }
+        }
+
+        if (count * 2 < robots.size()) continue;
+
+        std::cout << step << "\n";
+        return 0;
+      }
+    }
+
+    ++step;
+  }
+
+  std::cout << step << "\n";
   return 0;
 }
