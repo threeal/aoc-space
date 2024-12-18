560ed0013575fddaba0609ad8a69ef5edea5c60a
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-18 13:52:04 +0700

    feat(day-18): solve first puzzle by using breadth-first search
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- day-18/puzzle-1.cpp
+++ day-18/puzzle-1.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <queue>
 #include <string>
+#include <tuple>
 #include <vector>
 
 int main() {
-  std::vector<std::vector<bool>> map(7, std::vector<bool>(7, false));
+  std::vector<std::vector<bool>> map(71, std::vector<bool>(71, false));
 
-  for (int i{12}; i > 0; --i) {
+  for (int i{1024}; i > 0; --i) {
     std::string line{};
     if (std::getline(std::cin, line)) {
       std::size_t i{0}, x{0}, y{0};
@@ -24,12 +26,40 @@ int main() {
     }
   }
 
-  for (const auto& row : map) {
-    for (const auto obstacle : row) {
-      std::cout << (obstacle ? "# " : ". ");
+  std::queue<std::tuple<std::size_t, std::size_t>> queue{};
+  queue.push({0, 0});
+
+  for (int steps{0}; !queue.empty(); ++steps) {
+    for (std::size_t n{queue.size()}; n > 0; --n) {
+      const auto [x, y] = queue.front();
+      queue.pop();
+
+      if (x == 70 && y == 70) {
+        std::cout << steps << "\n";
+        return 0;
+      }
+
+      if (x < 70 && !map[y][x + 1]) {
+        map[y][x + 1] = true;
+        queue.push({x + 1, y});
+      }
+
+      if (y < 70 && !map[y + 1][x]) {
+        map[y + 1][x] = true;
+        queue.push({x, y + 1});
+      }
+
+      if (x > 0 && !map[y][x - 1]) {
+        map[y][x - 1] = true;
+        queue.push({x - 1, y});
+      }
+
+      if (y > 0 && !map[y - 1][x]) {
+        map[y - 1][x] = true;
+        queue.push({x, y - 1});
+      }
     }
-    std::cout << "\n";
   }
 
-  return 0;
+  return 1;
 }
