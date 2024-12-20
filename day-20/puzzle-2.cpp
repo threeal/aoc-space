d05f711a214e924e15761f5c51d6acd1e1ac0c20
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-20 19:07:56 +0700

    feat(day-20): solve second puzzle by using double BFS
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- day-20/puzzle-2.cpp
+++ day-20/puzzle-2.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 
 int main() {
-  int time{1};
+  int time{0};
   std::queue<std::tuple<std::size_t, std::size_t>> steps{};
   std::vector<std::vector<int>> map{};
 
@@ -18,7 +18,7 @@ int main() {
           break;
 
         case 'S':
-          row[i] = time;
+          row[i] = time + 1;
           steps.push({map.size(), i});
           break;
 
@@ -37,64 +37,65 @@ int main() {
       const auto [y, x] = steps.front();
       steps.pop();
 
-      switch (map[y - 1][x]) {
-        case -1:
-          if (y >= 2) {
-            const int t{map[y - 2][x]};
-            if (t > 0 && time - t - 3 >= 100) ++count;
-          }
-          break;
-
-        case 0:
-          map[y - 1][x] = time;
-          steps.push({y - 1, x});
-          break;
+      if (map[y - 1][x] == 0) {
+        map[y - 1][x] = time + 1;
+        steps.push({y - 1, x});
       }
 
-      switch (map[y][x - 1]) {
-        case -1:
-          if (x >= 2) {
-            const int t{map[y][x - 2]};
-            if (t > 0 && time - t - 3 >= 100) ++count;
-          }
-          break;
-
-        case 0:
-          map[y][x - 1] = time;
-          steps.push({y, x - 1});
-          break;
+      if (map[y][x - 1] == 0) {
+        map[y][x - 1] = time + 1;
+        steps.push({y, x - 1});
       }
 
-      switch (map[y + 1][x]) {
-        case -1:
-          if (y + 2 < map.size()) {
-            const int t{map[y + 2][x]};
-            if (t > 0 && time - t - 3 >= 100) ++count;
-          }
-          break;
-
-        case 0:
-          map[y + 1][x] = time;
-          steps.push({y + 1, x});
-          break;
+      if (map[y + 1][x] == 0) {
+        map[y + 1][x] = time + 1;
+        steps.push({y + 1, x});
       }
 
-      switch (map[y][x + 1]) {
-        case -1:
-          if (x + 2 < map[y].size()) {
-            const int t{map[y][x + 2]};
-            if (t > 0 && time - t - 3 >= 100) ++count;
-          }
-          break;
+      if (map[y][x + 1] == 0) {
+        map[y][x + 1] = time + 1;
+        steps.push({y, x + 1});
+      }
 
-        case 0:
-          map[y][x + 1] = time;
-          steps.push({y, x + 1});
-          break;
+      std::vector<std::vector<bool>> visited(
+          map.size(), std::vector<bool>(map.front().size(), false));
+
+      std::queue<std::tuple<std::size_t, std::size_t>> steps{};
+
+      visited[y][x] = true;
+      steps.push({y, x});
+
+      for (int i{0}; i <= 20 && !steps.empty(); ++i) {
+        for (std::size_t n{steps.size()}; n > 0; --n) {
+          const auto [y, x] = steps.front();
+          steps.pop();
+
+          if (map[y][x] > 0 && time - map[y][x] - i >= 100) ++count;
+
+          if (y > 0 && !visited[y - 1][x]) {
+            visited[y - 1][x] = true;
+            steps.push({y - 1, x});
+          }
+
+          if (x > 0 && !visited[y][x - 1]) {
+            visited[y][x - 1] = true;
+            steps.push({y, x - 1});
+          }
+
+          if (y + 1 < visited.size() && !visited[y + 1][x]) {
+            visited[y + 1][x] = true;
+            steps.push({y + 1, x});
+          }
+
+          if (x + 1 < visited[y].size() && !visited[y][x + 1]) {
+            visited[y][x + 1] = true;
+            steps.push({y, x + 1});
+          }
+        }
       }
     }
   }
 
   std::cout << count << "\n";
-  return 1;
+  return 0;
 }
