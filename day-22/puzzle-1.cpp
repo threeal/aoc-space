af8d212a3ab66293c1888dae3ec2cbe9245c5148
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-22 12:35:44 +0700

    feat(day-22): solve first puzzle by using for-loop to calculate secret numbers
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- day-22/puzzle-1.cpp
+++ day-22/puzzle-1.cpp
@@ -2,10 +2,24 @@
 #include <string>
 
 int main() {
+  std::size_t sum{0};
+
   std::string line{};
   while (std::getline(std::cin, line)) {
-    int secret{std::stoi(line)};
-    std::cout << secret << "\n";
+    std::size_t secret{};
+    for (const auto c : line) {
+      secret = secret * 10 + c - '0';
+    }
+
+    for (std::size_t n{2000}; n > 0; --n) {
+      secret = (secret ^ (secret * 64)) % 16777216;
+      secret = (secret ^ (secret / 32)) % 16777216;
+      secret = (secret ^ (secret * 2048)) % 16777216;
+    }
+
+    sum += secret;
   }
-  return 1;
+
+  std::cout << sum << "\n";
+  return 0;
 }
