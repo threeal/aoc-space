b3df5c0527c709fd3e69a9b43aa004f9f106e481
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-12-22 14:10:11 +0700

    feat(day-22): solve second puzzle by collecting all pattern price possibilities
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- day-22/puzzle-2.cpp
+++ day-22/puzzle-2.cpp
@@ -1,25 +1,57 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
 
 int main() {
-  std::size_t sum{0};
+  std::unordered_map<std::size_t, long long> patternPrices{};
 
   std::string line{};
   while (std::getline(std::cin, line)) {
-    std::size_t secret{};
+    long long secret{};
     for (const auto c : line) {
       secret = secret * 10 + c - '0';
     }
 
-    for (std::size_t n{2000}; n > 0; --n) {
+    std::unordered_set<std::size_t> seenPatterns{};
+    std::size_t pattern{0};
+
+    for (std::size_t i{1}; i < 4; ++i) {
+      const char prevPrice = secret % 10;
       secret = (secret ^ (secret * 64)) % 16777216;
       secret = (secret ^ (secret / 32)) % 16777216;
       secret = (secret ^ (secret * 2048)) % 16777216;
+      const char price = secret % 10;
+
+      pattern = (pattern * 20) + 10 + (price - prevPrice);
     }
 
-    sum += secret;
+    for (std::size_t n{2000 - 3}; n > 0; --n) {
+      const char prevPrice = secret % 10;
+      secret = (secret ^ (secret * 64)) % 16777216;
+      secret = (secret ^ (secret / 32)) % 16777216;
+      secret = (secret ^ (secret * 2048)) % 16777216;
+      const char price = secret % 10;
+
+      pattern = ((pattern * 20) + 10 + (price - prevPrice)) % 160000;
+      if (!seenPatterns.contains(pattern)) {
+        seenPatterns.insert(pattern);
+
+        auto it = patternPrices.find(pattern);
+        if (it == patternPrices.end()) {
+          patternPrices.emplace(pattern, price);
+        } else {
+          it->second += price;
+        }
+      }
+    }
   }
 
-  std::cout << sum << "\n";
-  return 1;
+  long long maxPrice{0};
+  for (const auto& [pattern, price] : patternPrices) {
+    if (price > maxPrice) maxPrice = price;
+  }
+  std::cout << maxPrice << "\n";
+
+  return 0;
 }
