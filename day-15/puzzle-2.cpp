6403c67650c38e5a3280d9e5f712fa4669308a89
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-23 16:39:39 +0700

    feat(day-15): solve second puzzle by simulating robot movement
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- day-15/puzzle-2.cpp
+++ day-15/puzzle-2.cpp
@@ -2,24 +2,77 @@
 #include <string>
 #include <vector>
 
+static bool checkBoxes(
+    const std::vector<std::string>& lines,
+    std::size_t y, std::size_t x, std::size_t dir) {
+  if (lines[y][x] == '#' || lines[y][x + 1] == '#') return false;
+  if (lines[y][x] == '[') return checkBoxes(lines, y + dir, x, dir);
+  if (lines[y][x] == ']' && !checkBoxes(lines, y + dir, x - 1, dir)) {
+    return false;
+  }
+  if (lines[y][x + 1] == '[' && !checkBoxes(lines, y + dir, x + 1, dir)) {
+    return false;
+  }
+  return true;
+}
+
+static void moveBoxes(
+    std::vector<std::string>& lines,
+    std::size_t y, std::size_t x, std::size_t dir) {
+  if (lines[y][x] == '[') {
+    moveBoxes(lines, y + dir, x, dir);
+    lines[y + dir][x] = '[';
+    lines[y + dir][x + 1] = ']';
+    lines[y][x] = '.';
+    lines[y][x + 1] = '.';
+  } else {
+    if (lines[y][x] == ']') {
+      moveBoxes(lines, y + dir, x - 1, dir);
+      lines[y + dir][x - 1] = '[';
+      lines[y + dir][x] = ']';
+      lines[y][x - 1] = '.';
+      lines[y][x] = '.';
+    }
+    if (lines[y][x + 1] == '[') {
+      moveBoxes(lines, y + dir, x + 1, dir);
+      lines[y + dir][x + 1] = '[';
+      lines[y + dir][x + 2] = ']';
+      lines[y][x + 1] = '.';
+      lines[y][x + 2] = '.';
+    }
+  }
+}
+
 int main() {
+  std::size_t ry{0}, rx{0};
+
   std::string line{};
   std::vector<std::string> lines{};
   while (std::getline(std::cin, line) && !line.empty()) {
-    lines.push_back(line);
-  }
+    const std::size_t n{line.size()};
+    line.resize(2 * n);
 
-  std::size_t ry{0}, rx{0};
-  for (std::size_t y{0}; y < lines.size(); ++y) {
-    for (std::size_t x{0}; x < lines[y].size(); ++x) {
-      if (lines[y][x] == '@') {
-        lines[y][x] = '.';
-        ry = y;
-        rx = x;
-        y = lines.size();
-        x = lines[y].size();
+    for (std::size_t i{n}; i > 0; --i) {
+      const std::size_t ii{i - 1};
+      switch (line[ii]) {
+        case 'O':
+          line[2 * ii] = '[';
+          line[2 * ii + 1] = ']';
+          break;
+
+        case '@':
+          ry = lines.size();
+          rx = ii * 2;
+          line[ii] = '.';
+
+        default:
+          line[2 * ii] = line[ii];
+          line[2 * ii + 1] = line[ii];
+          break;
       }
     }
+
+    lines.push_back(line);
   }
 
   while (std::getline(std::cin, line)) {
@@ -27,37 +80,69 @@ int main() {
       std::size_t x{}, y{};
       switch (c) {
         case '^':
-          y = ry - 1;
-          while (lines[y][rx] == 'O') --y;
-          if (lines[y][rx] == '.') {
-            lines[y][rx] = 'O';
-            lines[--ry][rx] = '.';
+          switch (lines[ry - 1][rx]) {
+            case '.':
+              --ry;
+              break;
+
+            case ']':
+              if (checkBoxes(lines, ry - 1, rx - 1, -1)) {
+                moveBoxes(lines, ry - 1, rx - 1, -1);
+                --ry;
+              }
+              break;
+
+            case '[':
+              if (checkBoxes(lines, ry - 1, rx, -1)) {
+                moveBoxes(lines, ry - 1, rx, -1);
+                --ry;
+              }
+              break;
           }
           break;
 
         case 'v':
-          y = ry + 1;
-          while (lines[y][rx] == 'O') ++y;
-          if (lines[y][rx] == '.') {
-            lines[y][rx] = 'O';
-            lines[++ry][rx] = '.';
+          switch (lines[ry + 1][rx]) {
+            case '.':
+              ++ry;
+              break;
+
+            case ']':
+              if (checkBoxes(lines, ry + 1, rx - 1, 1)) {
+                moveBoxes(lines, ry + 1, rx - 1, 1);
+                ++ry;
+              }
+              break;
+
+            case '[':
+              if (checkBoxes(lines, ry + 1, rx, 1)) {
+                moveBoxes(lines, ry + 1, rx, 1);
+                ++ry;
+              }
+              break;
           }
           break;
 
         case '<':
           x = rx - 1;
-          while (lines[ry][x] == 'O') --x;
+          while (lines[ry][x] == ']') x -= 2;
           if (lines[ry][x] == '.') {
-            lines[ry][x] = 'O';
+            while (x < rx - 1) {
+              lines[ry][x++] = '[';
+              lines[ry][x++] = ']';
+            }
             lines[ry][--rx] = '.';
           }
           break;
 
         case '>':
           x = rx + 1;
-          while (lines[ry][x] == 'O') ++x;
+          while (lines[ry][x] == '[') x += 2;
           if (lines[ry][x] == '.') {
-            lines[ry][x] = 'O';
+            while (x > rx + 1) {
+              lines[ry][x--] = ']';
+              lines[ry][x--] = '[';
+            }
             lines[ry][++rx] = '.';
           }
           break;
@@ -68,7 +153,7 @@ int main() {
   std::size_t sum{0};
   for (std::size_t y{0}; y < lines.size(); ++y) {
     for (std::size_t x{0}; x < lines[y].size(); ++x) {
-      if (lines[y][x] == 'O') sum += 100 * y + x;
+      if (lines[y][x] == '[') sum += 100 * y + x;
     }
   }
 
