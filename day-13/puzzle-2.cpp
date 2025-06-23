d3d3ade1b5ec49268ea51c35f56f4e5f165b15f1
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-06-23 12:54:32 +0700

    feat(day-13): solve second puzzle by using Cramer's rule
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- day-13/puzzle-2.cpp
+++ day-13/puzzle-2.cpp
@@ -2,17 +2,17 @@
 #include <string>
 
 int main() {
-  int tokens{0};
+  long long tokens{0};
   std::string line{};
   while (std::getline(std::cin, line)) {
     std::size_t i{12};
-    int ax{0};
+    long long ax{0};
     while (line[i] != ',') {
       ax = ax * 10 + line[i] - '0';
       ++i;
     }
     i += 4;
-    int ay{0};
+    long long ay{0};
     while (i < line.size()) {
       ay = ay * 10 + line[i] - '0';
       ++i;
@@ -20,13 +20,13 @@ int main() {
 
     if (!std::getline(std::cin, line)) break;
     i = 12;
-    int bx{0};
+    long long bx{0};
     while (line[i] != ',') {
       bx = bx * 10 + line[i] - '0';
       ++i;
     }
     i += 4;
-    int by{0};
+    long long by{0};
     while (i < line.size()) {
       by = by * 10 + line[i] - '0';
       ++i;
@@ -34,25 +34,28 @@ int main() {
 
     if (!std::getline(std::cin, line)) break;
     i = 9;
-    int px{0};
+    long long px{};
     while (line[i] != ',') {
       px = px * 10 + line[i] - '0';
       ++i;
     }
     i += 4;
-    int py{0};
+    long long py{0};
     while (i < line.size()) {
       py = py * 10 + line[i] - '0';
       ++i;
     }
 
-    for (int b{std::min(100, px / bx)}; b >= 0; --b) {
-      const int dx{px - b * bx};
-      const int dy{py - b * by};
-      if (dx % ax == 0 && dy % ay == 0 && dx / ax == dy / ay) {
-        if (dx / ax <= 100) {
-          tokens += b + 3 * dx / ax;
-          break;
+    px += 10000000000000;
+    py += 10000000000000;
+
+    const long long det{ax * by - bx * ay};
+    if (det != 0) {
+      const long long a{px * by - bx * py};
+      if (a % det == 0) {
+        const long long b{ax * py - px * ay};
+        if (b % det == 0) {
+          tokens += 3 * a / det + b / det;
         }
       }
     }
