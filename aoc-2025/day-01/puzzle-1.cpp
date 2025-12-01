271564430561db652d3343ad8564dc1b0a7e13b6
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-01 13:14:01 +0700

    feat(aoc-2025/day-1): solve by using modulo operation
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- aoc-2025/day-01/puzzle-1.cpp
+++ aoc-2025/day-01/puzzle-1.cpp
@@ -2,8 +2,22 @@
 #include <string>
 
 int main() {
+  int count{0}, pos{50};
+
   std::string line{};
   while (std::getline(std::cin, line)) {
+    int move{0};
+    for (std::size_t i{1}; i < line.size(); ++i) {
+      move = move * 10 + line[i] - '0';
+    }
+
+    if (line[0] == 'L') move *= -1;
+
+    pos = (100 + (move + pos) % 100) % 100;
+    if (pos == 0) ++count;
   }
-  return 1;
+
+  std::cout << count << "\n";
+
+  return 0;
 }
