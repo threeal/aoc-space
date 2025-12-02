98a9ec95571efcc037268453faddb34e1c884694
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-02 12:36:28 +0700

    feat(aoc-2024/day-23): solve second puzzle by using backtracking
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- aoc-2024/day-23/puzzle-2.cpp
+++ aoc-2024/day-23/puzzle-2.cpp
@@ -2,10 +2,16 @@
 #include <string>
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
+
+using Connections =
+    std::unordered_map<std::string, std::unordered_set<std::string>>;
+
+static std::string find(
+    const Connections& connections, std::vector<std::string>& computers);
 
 int main() {
-  int count{0};
-  std::unordered_map<std::string, std::unordered_set<std::string>> connections{};
+  Connections connections{};
 
   std::string line{};
   while (std::getline(std::cin, line)) {
@@ -13,15 +19,54 @@ int main() {
     const auto a = line.substr(0, delimiter);
     const auto b = line.substr(delimiter + 1);
 
-    for (const auto& c : connections[a]) {
-      if (a[0] != 't' && b[0] != 't' && c[0] != 't') continue;
-      if (connections[c].contains(b)) ++count;
-    }
-
     connections[a].insert(b);
     connections[b].insert(a);
   }
 
-  std::cout << count << "\n";
-  return 1;
+  std::string longestPassword{""};
+  std::vector<std::string> computers{};
+  for (const auto& [computer, _] : connections) {
+    computers.push_back(computer);
+    const std::string password{find(connections, computers)};
+    if (password.size() > longestPassword.size()) longestPassword = password;
+    computers.pop_back();
+  }
+
+  std::cout << longestPassword << "\n";
+
+  return 0;
+}
+
+static std::string find(
+    const Connections& connections, std::vector<std::string>& computers) {
+  std::string longestPassword{computers[0]};
+  for (std::size_t i{1}; i < computers.size(); ++i) {
+    longestPassword += "," + computers[i];
+  }
+
+  const auto it = connections.find(computers.back());
+  if (it != connections.end()) {
+    for (const std::string& nextComputer : it->second) {
+      if (nextComputer > computers.back()) {
+        std::size_t i{0};
+        while (i < computers.size()) {
+          const auto it = connections.find(computers[i]);
+          if (it == connections.end()) break;
+          if (!it->second.contains(nextComputer)) break;
+          ++i;
+        }
+
+        if (i == computers.size()) {
+          computers.push_back(nextComputer);
+          const std::string& password(find(connections, computers));
+          if (password.size() > longestPassword.size()) {
+            longestPassword = password;
+          }
+          computers.pop_back();
+        }
+      }
+    }
+  }
+
+  return longestPassword;
 }
