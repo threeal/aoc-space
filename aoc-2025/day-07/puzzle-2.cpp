193739f515b00c7d072b2fbf43864b2936a1f4d9
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-07 14:28:14 +0700

    feat(aoc-2025/day-7): solve second puzzle by using bottom-up DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- aoc-2025/day-07/puzzle-2.cpp
+++ aoc-2025/day-07/puzzle-2.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <stack>
 #include <string>
+#include <vector>
 
 int main() {
+  std::stack<std::string> lines{};
   std::string line{};
-  while (std::getline(std::cin, line)) {
+  while (std::getline(std::cin, line)) lines.push(line);
+
+  std::vector<long long> timelines(lines.top().size(), 1);
+  while (lines.size() > 1) {
+    for (std::size_t i{0}; i < lines.top().size(); ++i) {
+      if (lines.top()[i] == '^') {
+        timelines[i] = timelines[i - 1] + timelines[i + 1];
+      }
+    }
+    lines.pop();
   }
-  return 1;
+
+  long long total{0};
+  for (std::size_t i{0}; i < lines.top().size(); ++i) {
+    if (lines.top()[i] == 'S') total += timelines[i];
+  }
+
+  std::cout << total << '\n';
+
+  return 0;
 }
