a72b931fd1317097b25f1b1236002ec0675b7200
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-07 13:26:39 +0700

    feat(aoc-2025/day-7): solve first puzzle by using BFS
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- aoc-2025/day-07/puzzle-1.cpp
+++ aoc-2025/day-07/puzzle-1.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
+#include <queue>
 #include <string>
 
 int main() {
+  int count{0};
+
   std::string line{};
-  while (std::getline(std::cin, line)) {
+  if (std::getline(std::cin, line)) {
+    std::queue<std::size_t> beams{};
+    for (std::size_t i{0}; i < line.size(); ++i) {
+      if (line[i] == 'S') beams.push(i);
+    }
+
+    while (std::getline(std::cin, line)) {
+      for (std::size_t n{beams.size()}; n > 0; --n) {
+        const std::size_t beam{beams.front()};
+        beams.pop();
+
+        switch (line[beam]) {
+          case '^':
+            ++count;
+
+            if (line[beam - 1] != 'X') {
+              line[beam - 1] = 'X';
+              beams.push(beam - 1);
+            }
+
+            line[beam + 1] = 'X';
+            beams.push(beam + 1);
+            break;
+
+          case 'X':
+            break;
+
+          default:
+            line[beam] = 'X';
+            beams.push(beam);
+        }
+      }
+    }
   }
 
-  return 1;
+  std::cout << count << "\n";
+
+  return 0;
 }
