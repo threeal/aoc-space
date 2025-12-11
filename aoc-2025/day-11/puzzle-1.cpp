277183795c666a8f98c1e661c81b012425e60129
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-12-11 12:45:06 +0700

    feat(aoc-2025/day-11): solve first puzzle by using DP
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- aoc-2025/day-11/puzzle-1.cpp
+++ aoc-2025/day-11/puzzle-1.cpp
@@ -1,10 +1,56 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
-int main() {
-  std::string line{};
-  while (std::getline(std::cin, line)) {
+static int explore(
+    std::unordered_map<std::string, int>& cache,
+    const std::unordered_map<std::string, std::vector<std::string>>& devices,
+    const std::string& device) {
+  auto cacheIt = cache.find(device);
+  if (cacheIt == cache.end()) {
+    int count{0};
+    const auto it = devices.find(device);
+    if (it != devices.end()) {
+      for (const std::string& next : it->second) {
+        if (next == "out") {
+          ++count;
+        } else {
+          count += explore(cache, devices, next);
+        }
+      }
+    }
+
+    cacheIt = cache.emplace(device, count).first;
   }
 
-  return 1;
+  return cacheIt->second;
+}
+
+int main() {
+  std::unordered_map<std::string, std::vector<std::string>> devices{};
+
+  std::string line{};
+  while (std::getline(std::cin, line)) {
+    std::size_t l{0}, r{0};
+    while (line[r] != ':') ++r;
+    const std::string device{line.substr(l, r - l)};
+
+    std::vector<std::string> nexts{};
+
+    r += 2;
+    while (r < line.size()) {
+      l = r;
+      while (r < line.size() && line[r] != ' ') ++r;
+      nexts.push_back(std::move(line.substr(l, r - l)));
+      ++r;
+    }
+
+    devices.emplace(std::move(device), std::move(nexts));
+  }
+
+  std::unordered_map<std::string, int> cache{};
+  std::cout << explore(cache, devices, "you") << "\n";
+
+  return 0;
 }
